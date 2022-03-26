83e8138f0ca22cd335f9cdc2fb6698f3b22ca71f
genuinestalwart <genuinestalwart@gmail.com>
2022-03-26 09:06:57 +0600

    header added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Header = () => {
     return (
-        <header><h1>TCB Online Sell</h1></header>
+        <header className='bg-violet-300 flex h-20 items-center justify-center'><h1 className='font-bold text-4xl text-indigo-500'>TCB Online Sell</h1></header>
     );
 };
 
